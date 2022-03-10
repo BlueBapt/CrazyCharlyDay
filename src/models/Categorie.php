4dc78c39fd7c63ea0c7175ab6d7bb8cbda8eb918
Baptiste ROBERT <bluebapt@gmail.com>
2022-03-10 10:46:41 +0100

    models v2

--- src/models/Categorie.php
+++ src/models/Categorie.php
@@ -2,7 +2,14 @@
 
 namespace custumbox\models;
 
-class Categorie
-{
+class Categorie extends \Illuminate\Database\Eloquent\Model {
 
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+
+    public function produit() {
+        return $this->hasMany('custumbox\models\Produit', 'id');
+    }
 }
--- src/models/Produit.php
+++ src/models/Produit.php
@@ -2,7 +2,13 @@
 
 namespace custumbox\models;
 
-class Produit
-{
+class Produit extends \Illuminate\Database\Eloquent\Model {
 
+    protected $table = 'produit';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function categorie() {
+        return $this->belongsTo('custumbox\models\Categorie', 'id');
+    }
 }
